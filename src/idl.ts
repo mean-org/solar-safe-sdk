9f935d307d906a0927f114bce848eb5b4223ce9a
Yansel <yanselfgok@gmail.com>
2022-04-27 12:08:44 -0400

    Adding IDL definition and types.

---
+++ src/idl.ts
@@ -0,0 +1,631 @@
+import { Idl } from "@project-serum/anchor";
+
+const idl: Idl = {
+  version: "0.12.0",
+  name: "mean_multisig",
+  instructions: [
+    {
+      name: "createMultisig",
+      accounts: [
+        {
+          name: "proposer",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "multisigOpsAccount",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [
+        {
+          name: "owners",
+          type: {
+            vec: {
+              defined: "Owner",
+            },
+          },
+        },
+        {
+          name: "threshold",
+          type: "u64",
+        },
+        {
+          name: "nonce",
+          type: "u8",
+        },
+        {
+          name: "label",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "editMultisig",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "multisigSigner",
+          isMut: false,
+          isSigner: true,
+        },
+      ],
+      args: [
+        {
+          name: "owners",
+          type: {
+            vec: {
+              defined: "Owner",
+            },
+          },
+        },
+        {
+          name: "threshold",
+          type: "u64",
+        },
+        {
+          name: "label",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "createTransaction",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transaction",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "transactionDetail",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "proposer",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "multisigOpsAccount",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [
+        {
+          name: "pid",
+          type: "publicKey",
+        },
+        {
+          name: "accs",
+          type: {
+            vec: {
+              defined: "TransactionAccount",
+            },
+          },
+        },
+        {
+          name: "data",
+          type: "bytes",
+        },
+        {
+          name: "operation",
+          type: "u8",
+        },
+        {
+          name: "title",
+          type: "string",
+        },
+        {
+          name: "description",
+          type: "string",
+        },
+        {
+          name: "expirationDate",
+          type: "u64",
+        },
+        {
+          name: "pdaTimestamp",
+          type: "u64",
+        },
+        {
+          name: "pdaBump",
+          type: "u8",
+        },
+      ],
+    },
+    {
+      name: "cancelTransaction",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transaction",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transactionDetail",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "proposer",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [],
+    },
+    {
+      name: "approve",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transaction",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transactionDetail",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "owner",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [],
+    },
+    {
+      name: "executeTransaction",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "multisigSigner",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "transaction",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transactionDetail",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "payer",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [],
+    },
+    {
+      name: "executeTransactionPda",
+      accounts: [
+        {
+          name: "multisig",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "multisigSigner",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "pdaAccount",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transaction",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "transactionDetail",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "payer",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+      ],
+      args: [
+        {
+          name: "pdaTimestamp",
+          type: "u64",
+        },
+        {
+          name: "pdaBump",
+          type: "u8",
+        },
+      ],
+    },
+  ],
+  accounts: [
+    {
+      name: "Multisig",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "owners",
+            type: {
+              vec: "publicKey",
+            },
+          },
+          {
+            name: "threshold",
+            type: "u64",
+          },
+          {
+            name: "nonce",
+            type: "u8",
+          },
+          {
+            name: "ownerSetSeqno",
+            type: "u32",
+          },
+          {
+            name: "label",
+            type: "string",
+          },
+          {
+            name: "createdOn",
+            type: "u64",
+          },
+          {
+            name: "pendingTxs",
+            type: "u64",
+          },
+        ],
+      },
+    },
+    {
+      name: "MultisigV2",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "owners",
+            type: {
+              array: [
+                {
+                  defined: "OwnerData",
+                },
+                10,
+              ],
+            },
+          },
+          {
+            name: "version",
+            type: "u8",
+          },
+          {
+            name: "nonce",
+            type: "u8",
+          },
+          {
+            name: "label",
+            type: {
+              array: ["u8", 32],
+            },
+          },
+          {
+            name: "ownerSetSeqno",
+            type: "u32",
+          },
+          {
+            name: "threshold",
+            type: "u64",
+          },
+          {
+            name: "pendingTxs",
+            type: "u64",
+          },
+          {
+            name: "createdOn",
+            type: "u64",
+          },
+        ],
+      },
+    },
+    {
+      name: "Transaction",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "multisig",
+            type: "publicKey",
+          },
+          {
+            name: "programId",
+            type: "publicKey",
+          },
+          {
+            name: "accounts",
+            type: {
+              vec: {
+                defined: "TransactionAccount",
+              },
+            },
+          },
+          {
+            name: "data",
+            type: "bytes",
+          },
+          {
+            name: "signers",
+            type: {
+              vec: "bool",
+            },
+          },
+          {
+            name: "ownerSetSeqno",
+            type: "u32",
+          },
+          {
+            name: "createdOn",
+            type: "u64",
+          },
+          {
+            name: "executedOn",
+            type: "u64",
+          },
+          {
+            name: "operation",
+            type: "u8",
+          },
+          {
+            name: "keypairs",
+            type: {
+              vec: {
+                array: ["u8", 64],
+              },
+            },
+          },
+          {
+            name: "proposer",
+            type: "publicKey",
+          },
+          {
+            name: "pdaTimestamp",
+            type: "u64",
+          },
+          {
+            name: "pdaBump",
+            type: "u8",
+          },
+        ],
+      },
+    },
+    {
+      name: "TransactionDetail",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "title",
+            type: {
+              array: ["u8", 64],
+            },
+          },
+          {
+            name: "description",
+            type: {
+              array: ["u8", 512],
+            },
+          },
+          {
+            name: "expirationDate",
+            type: "u64",
+          },
+        ],
+      },
+    },
+    {
+      name: "Owner",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "address",
+            type: "publicKey",
+          },
+          {
+            name: "name",
+            type: "string",
+          },
+        ],
+      },
+    },
+  ],
+  types: [
+    {
+      name: "OwnerData",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "address",
+            type: "publicKey",
+          },
+          {
+            name: "name",
+            type: {
+              array: ["u8", 32],
+            },
+          },
+        ],
+      },
+    },
+    {
+      name: "TransactionAccount",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "pubkey",
+            type: "publicKey",
+          },
+          {
+            name: "isSigner",
+            type: "bool",
+          },
+          {
+            name: "isWritable",
+            type: "bool",
+          },
+        ],
+      },
+    },
+  ],
+  errors: [
+    {
+      code: 6000,
+      name: "InvalidOwner",
+      msg: "The given owner is not part of this multisig.",
+    },
+    {
+      code: 6001,
+      name: "InvalidOwnersLen",
+      msg: "Owners length must be non zero.",
+    },
+    {
+      code: 6002,
+      name: "NotEnoughSigners",
+      msg: "Not enough owners signed this transaction.",
+    },
+    {
+      code: 6003,
+      name: "TransactionAlreadySigned",
+      msg: "Cannot delete a transaction that has been signed by an owner.",
+    },
+    {
+      code: 6004,
+      name: "Overflow",
+      msg: "Operation overflow",
+    },
+    {
+      code: 6005,
+      name: "UnableToDelete",
+      msg: "Cannot delete a transaction the owner did not create.",
+    },
+    {
+      code: 6006,
+      name: "AlreadyExecuted",
+      msg: "The given transaction has already been executed.",
+    },
+    {
+      code: 6007,
+      name: "AlreadyExpired",
+      msg: "Transaction proposal has expired.",
+    },
+    {
+      code: 6008,
+      name: "InvalidThreshold",
+      msg: "Threshold must be less than or equal to the number of owners.",
+    },
+    {
+      code: 6009,
+      name: "UniqueOwners",
+      msg: "Owners must be unique.",
+    },
+    {
+      code: 6010,
+      name: "OwnerNameTooLong",
+      msg: "Owner name must have less than 32 bytes.",
+    },
+    {
+      code: 6011,
+      name: "InvalidMultisigNonce",
+      msg: "Multisig nonce is not valid.",
+    },
+    {
+      code: 6012,
+      name: "InvalidMultisigVersion",
+      msg: "Multisig version is not valid.",
+    },
+    {
+      code: 6013,
+      name: "InvalidOwnerSetSeqNumber",
+      msg: "Multisig owner set secuency number is not valid.",
+    },
+    {
+      code: 6014,
+      name: "InvalidMultisig",
+      msg: "Multisig account is not valid.",
+    },
+  ],
+};
+
+export default idl;
---
+++ src/types.ts
@@ -0,0 +1,114 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MEAN_MULTISIG_OPS = new PublicKey("3TD6SWY9M1mLY2kZWJNavPLhwXvcRsWdnZLRaMzERJBw");
+export const LAMPORTS_PER_SIG = 5000;
+export const DEFAULT_EXPIRATION_TIME_SECONDS = 604800;
+
+export enum MultisigTransactionStatus {
+  // No enough signatures
+  Pending = 0,
+  // Approved by the required amount of signers
+  Approved = 1,
+  // Successfully executed (didExecute = true)
+  Executed = 2,
+  // Rejected by any owner
+  Rejected = 3,
+  // Invalid owners set seq number
+  Voided = 4,
+  // Proposal has expired
+  Expired = 5,
+  //
+  Queued = 6,
+}
+
+export enum MULTISIG_ACTIONS {
+  createMultisig = 1,
+  editMultisig = 2,
+  createTransaction = 3,
+  cancelTransaction = 4,
+  approveTransaction = 5,
+  executeTransaction = 6,
+}
+
+export type MultisigTransactionFees = {
+  networkFee: number;
+  rentExempt: number;
+  multisigFee: number;
+};
+
+export type Multisig = {
+  id: PublicKey;
+  label: string;
+  description?: string;
+  authority: PublicKey;
+  owners: MultisigParticipant[];
+  threshold: number;
+  nounce: number;
+  ownerSeqNumber: number;
+  createdOnUtc: Date;
+  pendingTxsAmount: number;
+  version: number;
+};
+
+export type MultisigTransaction = {
+  id: PublicKey;
+  operation: number;
+  multisig: PublicKey;
+  programId: PublicKey;
+  signers: boolean[];
+  createdOn: Date;
+  executedOn: Date | undefined;
+  ownerSeqNumber: number;
+  status: MultisigTransactionStatus;
+  accounts: any[];
+  data: Buffer;
+  proposer: PublicKey | undefined;
+  pdaTimestamp: number | undefined;
+  pdaBump: number | undefined;
+  details: MultisigTransactionDetail;
+  didSigned: boolean;
+};
+
+export type MultisigParticipant = {
+  address: string;
+  name: string;
+};
+
+export type MultisigTransactionDetail = {
+  title: string;
+  description: string;
+  expirationDate: Date | undefined;
+};
+
+export type MultisigTransactionSummary = {
+  address: string;
+  operation: string;
+  multisig: string;
+  approvals: number;
+  createdOn: string;
+  executedOn: string;
+  status: string;
+  proposer: string;
+  title: string;
+  description: string;
+  expirationDate: string;
+  didSigned: boolean;
+  instruction: MultisigTransactionInstructionInfo;
+};
+
+export type MultisigTransactionInstructionInfo = {
+  programId: string;
+  accounts: InstructionAccountInfo[];
+  data: InstructionDataInfo[];
+};
+
+export type InstructionAccountInfo = {
+  index: number;
+  label: string;
+  value: string;
+};
+
+export type InstructionDataInfo = {
+  label: string;
+  value: string;
+};
